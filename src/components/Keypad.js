539f42b427bf964dfa0827de4fd6d64129a46f1a
Abiud kiplagat beren <berenabiud4@gmail.com>
2024-11-03 22:10:31 +0300

    completed

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,20 @@
 // Code Keypad Component Here
+import React from "react";
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  const handleChange = () => {
+    console.log('Entering password...');
+  };
+
+  return (
+    <div>
+      <input
+        type="password"
+        onChange={handleChange}  // Corrected to 'onChange'
+        placeholder="Enter your password" 
+      />
+    </div>
+  );
 }
 
 export default Keypad;
